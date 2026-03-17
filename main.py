e92f2a6a1ba4dcce00ba88a67d0aa63631d29b8c
Mowi3449 <wipmod23@htl-kaindorf.at>
2026-03-17 11:20:16 +0100

    Add simple addition function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Math Magician Main File
 # usage: math operator operand1 operand2
+
+
 def main():
   import sys
   print("Welcome to Math Magician!")
@@ -7,8 +9,13 @@ def main():
 
   op = sys.argv[1]
   a = float(sys.argv[2])
-  b = float(sys.argv[3])    
+  b = float(sys.argv[3])  
 
+  if op == "+":
+    result = add(a, b)
 
 if __name__ == "__main__":
     main()
+
+def add (a, b):
+  return a + b
