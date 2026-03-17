6b2944f172168b8bdd001c6889c1d7f0bdd1e82a
zacmid23 <zacmid23@htl-kaindorf.at>
2026-03-17 11:09:31 +0100

    Create project structure and main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Math Magician Main File
+# usage: math operator operand1 operand2
+def main():
+  import sys
+  print("Welcome to Math Magician!")
+
+
+  op = sys.argv[1]
+  a = float(sys.argv[2])
+  b = float(sys.argv[3])    
+
+
+if __name__ == "__main__":
+    main()
